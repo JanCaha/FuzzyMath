f38c1b6832b84708f0bcdea91d9d5d84ee162c5c
Jan Caha <jan.caha@outlook.com>
2020-01-27 11:11:48 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# pylint: skip-file
+from setuptools import setup, find_packages
+
+setup(name='FuzzyMath',
+      version='0.1.0',
+      description='Small lightweight library for Python (version >= 3.6) '
+                  'that performs basic Interval and Fuzzy Arithmetic.',
+      url='https://github.com/JanCaha/FuzzyMath',
+      author='Jan Caha',
+      author_email='jan.caha@outlook.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose']
+      )
