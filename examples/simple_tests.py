fb155549d62156a05145256f11e6b425393f842a
Jan Caha <jan.caha@outlook.com>
2020-10-01 21:01:02 +0200

    update examples

--- examples/simple_tests.py
+++ examples/simple_tests.py
@@ -1,6 +1,9 @@
+import math
+
 from FuzzyMath.class_interval import Interval
 from FuzzyMath.class_fuzzy_number import FuzzyNumber
 
+# define two fuzzy numbers
 a = FuzzyNumber(alphas=[0, 0.5, 1],
                 alpha_cuts=[Interval.infimum_supremum(1, 3),
                             Interval.infimum_supremum(1.5, 2.5),
@@ -12,31 +15,70 @@ b = FuzzyNumber(alphas=[0, 0.3, 0.6, 1],
                             Interval.infimum_supremum(1, 3),
                             Interval.infimum_supremum(2, 2)])
 
-print(a.get_alpha_cut(1))
-
-print(-1 in a)
-print(0 in a)
-print(1 in a)
-print(5 in a)
-print(7 in a)
-
+# one fuzzy number functions
+print("--- representation a ---")
 print(a)
+print(repr(a))
 
-print(a + 1)
-print(1 + a)
-print(a + a)
+print("\n")
+print("--- in operator ---")
+print("-1 in a: {}".format(-1 in a))
+print("0 in a: {}".format(0 in a))
+print("1 in a: {}".format(1 in a))
+print("5 in a: {}".format(5 in a))
+print("7 in a: {}".format(7 in a))
 
-print(a * 1)
-print(2 * a)
-print(a * a)
+print("\n")
+print("--- < > operators ---")
+print("-1 < a: {}".format(-1 < a))
+print("0 < a: {}".format(0 < a))
+print("1 < a: {}".format(1 < a))
+print("5 < a: {}".format(5 < a))
+print("7 < a: {}".format(7 < a))
 
-print(10 / a)
-print(a / 10)
+print("-1 > a: {}".format(-1 > a))
+print("0 > a: {}".format(0 > a))
+print("1 > a: {}".format(1 > a))
+print("5 > a: {}".format(5 > a))
+print("7 > a: {}".format(7 > a))
 
-print(a ** 2)
+print("\n")
+print("--- len function ---")
+print(len(a))
 
+print("\n")
+print("--- == operator ---")
+print("a == b: {}".format(a == b))
+print("a == a: {}".format(a == a))
 
-# FuzzyNumber._iterate_alphas(a, b, Interval.__add__)
-# FuzzyNumber._iterate_alphas(a, 1, Interval.__add__)
-# FuzzyNumber._iterate_alphas(2, b, Interval.__radd__)
+print("\n")
+print("--- math operators a and number ---")
+print("a + 1 : {}".format(repr(a + 1)))
+print("1 + a : {}".format(repr(1 + a)))
+print("a - 1 : {}".format(repr(a - 1)))
+print("1 - a : {}".format(repr(1 - a)))
+print("a * 2 : {}".format(repr(a * 2)))
+print("2 * a : {}".format(repr(2 * a)))
+print("a / 2 : {}".format(repr(a / 2)))
+print("2 / a : {}".format(repr(2 / a)))
 
+print("pow(a, 2) : {}".format(repr(pow(a, 2))))
+print("a ** 2 : {}".format(repr(a ** 2)))
+
+print("\n")
+print("--- math operators a and b ---")
+print("a + b : {}".format(repr(a + b)))
+print("b + a : {}".format(repr(b + a)))
+print("a - b : {}".format(repr(a - b)))
+print("b - a : {}".format(repr(b - a)))
+print("a * b : {}".format(repr(a * b)))
+print("b * a : {}".format(repr(b * a)))
+print("b / a : {}".format(repr(b / a)))
+
+print("\n")
+print("--- math ---")
+print("a + 5 * b : {}".format(repr(a + 5 * b)))
+
+print("\n")
+print("--- math functions ---")
+print("a.apply_function(math.sin) : {}".format(repr(a.apply_function(math.sin))))
