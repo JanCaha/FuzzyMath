94ab7b5055f607ffeb2adaa702af59430cb01ed9
Jan Caha <jan.caha@outlook.com>
2021-02-19 23:50:24 +0100

    change imports and make functions available from module

--- FuzzyMath/__init__.py
+++ FuzzyMath/__init__.py
@@ -1,7 +1,10 @@
-from FuzzyMath.class_interval import Interval
-from FuzzyMath.class_fuzzy_number import FuzzyNumber
+from .class_interval import Interval
+from .class_fuzzy_number import FuzzyNumber
 
-from FuzzyMath.fuzzynumber_comparisons import (possibility_exceedance, possibility_strict_exceedance,
-                                               necessity_exceedance, necessity_strict_exceedance,
-                                               possibility_undervaluation, necessity_undervaluation,
-                                               possibility_strict_undervaluation, necessity_strict_undervaluation)
+from .fuzzynumber_comparisons import (possibility_exceedance, possibility_strict_exceedance,
+                                      necessity_exceedance, necessity_strict_exceedance,
+                                      possibility_undervaluation, necessity_undervaluation,
+                                      possibility_strict_undervaluation, necessity_strict_undervaluation)
+
+from .fuzzymath_utils import set_precision, get_precision
+
