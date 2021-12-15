0ef025cc8e320f3338d80f2be61ebb00ec5a518d
Jan Caha <jan.caha@outlook.com>
2021-12-15 22:45:04 +0100

    change build process

--- setup.py
+++ setup.py
@@ -1,24 +1,4 @@
-# pylint: skip-file
-from setuptools import setup, find_packages
-import unittest
+from setuptools import setup
 
-
-def my_test_suite():
-    test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='test_*.py')
-    return test_suite
-
-
-setup(name='FuzzyMath',
-      version='0.3.2',
-      description='Small lightweight library for Python (version >= 3.6) '
-                  'that performs basic Interval and Fuzzy Arithmetic.',
-      url='https://github.com/JanCaha/FuzzyMath',
-      author='Jan Caha',
-      author_email='jan.caha@outlook.com',
-      license='MIT',
-      packages=find_packages(),
-      install_requires=['numpy'],
-      zip_safe=False,
-      test_suite='setup.my_test_suite'
-      )
+if __name__ == "__main__":
+    setup()
