d906034f4063990a1ad8ae57dc11369e3dd59dce
Jan Caha <jan.caha@outlook.com>
2021-12-15 22:44:38 +0100

    refactor into directory

---
+++ FuzzyMath/__init__.py
@@ -0,0 +1,16 @@
+"""
+Python package `FuzzyMath` is a small lightweight library for Python (version >= 3.6) that performs basic
+Interval and Fuzzy Arithmetic.
+.. include:: ./documentation.md
+"""
+
+from .class_interval import Interval
+from .class_fuzzy_number import FuzzyNumber
+
+from .fuzzynumber_comparisons import (possibility_exceedance, possibility_strict_exceedance,
+                                      necessity_exceedance, necessity_strict_exceedance,
+                                      possibility_undervaluation, necessity_undervaluation,
+                                      possibility_strict_undervaluation, necessity_strict_undervaluation)
+
+from .fuzzymath_utils import set_precision, get_precision
+
