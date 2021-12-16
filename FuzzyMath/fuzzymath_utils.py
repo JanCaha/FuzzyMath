20a12451a0911fb93637553406d67f3e3e0f1881
Jan Caha <jan.caha@outlook.com>
2021-12-16 09:59:59 +0100

    linting fixes

--- FuzzyMath/fuzzymath_utils.py
+++ FuzzyMath/fuzzymath_utils.py
@@ -1,31 +1,37 @@
 import warnings
-from typing import NoReturn
+from typing import Union
 
 from . import config
 
 
-def set_up_precision(precision: (float, int) = None, default_precision: int = 15) -> int:
+def set_up_precision(precision: Union[float, int] = None, default_precision: int = 15) -> int:
+    
     if precision is None:
         precision = default_precision
+        
     if not isinstance(precision, (int, float)):
         warnings.warn("`precision` is not either `int` or `float`. "
                       "Using default value of precision `{0}`."
                       .format(default_precision), UserWarning)
         precision = default_precision
+        
     if precision < 1 or precision > 15:
         raise ValueError("`precision` should be value from range `0` to `15`. It is `{0}`."
                          .format(precision))
+        
     if precision != round(precision, 0):
         warnings.warn("`precision` should be number without decimal part. Retyping to Integer.")
 
     return int(precision)
 
 
-def set_precision(precision: (float, int)) -> NoReturn:
+def set_precision(precision: Union[float, int]) -> None:
+    
     precision = set_up_precision(precision)
 
     config.precision = int(precision)
 
 
 def get_precision() -> int:
+    
     return config.precision
