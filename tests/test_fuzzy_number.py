c34b7665f07470a7bcd077bdb0b6c6d987f6c8d0
Jan Caha <jan.caha@outlook.com>
2022-03-21 22:00:19 +0100

    update tests with factories

--- tests/test_fuzzy_number.py
+++ tests/test_fuzzy_number.py
@@ -4,6 +4,7 @@ import math
 
 from FuzzyMath.class_fuzzy_number import FuzzyNumber
 from FuzzyMath.class_interval import Interval
+from FuzzyMath.class_factories import FuzzyNumberFactory, IntervalFactory
 
 
 def test_creation_errors():
@@ -43,72 +44,72 @@ def test_creation_errors():
     with pytest.raises(TypeError,
                        match="All elements of `alpha_cuts` must be Interval"):
         FuzzyNumber([0, 1],
-                    [Interval.two_values(0, 1), 5])
+                    [IntervalFactory.two_values(0, 1), 5])
 
     with pytest.raises(ValueError,
                        match="Interval on lower alpha level has to contain the higher level alpha cuts"):
         FuzzyNumber([0, 1],
-                    [Interval.two_values(0, 1),
-                        Interval.two_values(2, 2)])
+                    [IntervalFactory.two_values(0, 1),
+                     IntervalFactory.two_values(2, 2)])
 
     with pytest.raises(ValueError,
                        match="The fuzzy number is invalid"):
-        FuzzyNumber.triangular(5, 4, 3)
+        FuzzyNumberFactory.triangular(5, 4, 3)
 
     with pytest.raises(ValueError,
                        match="The fuzzy number is invalid"):
-        FuzzyNumber.trapezoidal(5, 1, 4, 3)
-    
+        FuzzyNumberFactory.trapezoidal(5, 1, 4, 3)
+
 
 def test_creation():
-    
-    assert isinstance(FuzzyNumber.triangular(1, 2, 3), FuzzyNumber)
-    assert isinstance(FuzzyNumber.triangular(1, 2, 3), FuzzyNumber)
-    assert isinstance(FuzzyNumber.triangular(1, 2, 3, number_of_cuts=10, precision=2), FuzzyNumber)
-    
-    assert isinstance(FuzzyNumber.trapezoidal(1, 2, 3, 4), FuzzyNumber)
-    assert isinstance(FuzzyNumber.trapezoidal(1, 2, 3, 4, number_of_cuts=10, precision=2), FuzzyNumber)
-    
-    assert isinstance(FuzzyNumber.crisp_number(0), FuzzyNumber)
-    assert isinstance(FuzzyNumber.crisp_number(0, precision=1), FuzzyNumber)
-    
-            
+
+    assert isinstance(FuzzyNumberFactory.triangular(1, 2, 3), FuzzyNumber)
+    assert isinstance(FuzzyNumberFactory.triangular(1, 2, 3), FuzzyNumber)
+    assert isinstance(FuzzyNumberFactory.triangular(1, 2, 3, number_of_cuts=10, precision=2), FuzzyNumber)
+
+    assert isinstance(FuzzyNumberFactory.trapezoidal(1, 2, 3, 4), FuzzyNumber)
+    assert isinstance(FuzzyNumberFactory.trapezoidal(1, 2, 3, 4, number_of_cuts=10, precision=2), FuzzyNumber)
+
+    assert isinstance(FuzzyNumberFactory.crisp_number(0), FuzzyNumber)
+    assert isinstance(FuzzyNumberFactory.crisp_number(0, precision=1), FuzzyNumber)
+
+
 def test_fuzzynumber_creation_string():
 
     string_fn = '(0.0;1.0,3.0)(0.111111111111111;1.1,2.9)(0.222222222222222;1.2,2.8)(0.333333333333333;1.3,2.7)' \
         '(0.444444444444444;1.4,2.6)(0.555555555555556;1.5,2.5)(0.666666666666667;1.6,2.4)' \
         '(0.777777777777778;1.7,2.3)(0.888888888888889;1.8,2.2)(1.0;2.0,2.0)'
 
-    assert isinstance(FuzzyNumber.parse_string(string_fn), FuzzyNumber)
+    assert isinstance(FuzzyNumberFactory.parse_string(string_fn), FuzzyNumber)
 
     string_fn = '(0.0;1,3)(0.5;1.9999,2.0001)(1.0;2,2)'
 
-    assert isinstance(FuzzyNumber.parse_string(string_fn), FuzzyNumber)
-    
-    assert isinstance(FuzzyNumber.parse_string(string_fn, precision=1), FuzzyNumber)
-    
+    assert isinstance(FuzzyNumberFactory.parse_string(string_fn), FuzzyNumber)
+
+    assert isinstance(FuzzyNumberFactory.parse_string(string_fn, precision=1), FuzzyNumber)
+
 
 def test_fuzzynumber_creation_string_errors():
-    
+
     with pytest.raises(ValueError,
                        match="Cannot parse FuzzyNumber from this definition"):
         string_fn = '(0.0;1.0,3.0)(0.5;1.9999)(1.0;2.0,2.0)'
-        FuzzyNumber.parse_string(string_fn)
+        FuzzyNumberFactory.parse_string(string_fn)
 
     with pytest.raises(ValueError,
                        match="element of Fuzzy Number"):
         string_fn = '(0.0;1.0,3.0)(0.5;2.0001,1.9999)(1.0;2.0,2.0)'
-        FuzzyNumber.parse_string(string_fn)
+        FuzzyNumberFactory.parse_string(string_fn)
 
     with pytest.raises(ValueError,
                        match="element of Fuzzy Number"):
         string_fn = '(0.0;1.0,3.0)(1.1;1.9999,2.0001)(1.0;2.0,2.0)'
-        FuzzyNumber.parse_string(string_fn)
+        FuzzyNumberFactory.parse_string(string_fn)
 
     with pytest.raises(ValueError,
                        match="Interval on lower alpha level has to contain the higher"):
         string_fn = '(0.0;1.0,3.0)(0.5;2.5,2.75)(1.0;2.0,2.0)'
-        FuzzyNumber.parse_string(string_fn)
+        FuzzyNumberFactory.parse_string(string_fn)
 
 
 def test_alphas(fn_a: FuzzyNumber, fn_e: FuzzyNumber):
@@ -117,41 +118,41 @@ def test_alphas(fn_a: FuzzyNumber, fn_e: FuzzyNumber):
 
 
 def test_alpha_cuts(fn_a: FuzzyNumber):
-    
-    intervals = [Interval.infimum_supremum(1, 3),
-                 Interval.infimum_supremum(2, 2)]
-    
+
+    intervals = [IntervalFactory.infimum_supremum(1, 3),
+                 IntervalFactory.infimum_supremum(2, 2)]
+
     assert intervals == fn_a.alpha_cuts
 
 
 def test_get_alpha_cut(fn_a: FuzzyNumber):
-    
-    assert fn_a.get_alpha_cut(0) == Interval.two_values(1, 3)
-    assert fn_a.get_alpha_cut(0.25) == Interval.two_values(1.25, 2.75)
-    assert fn_a.get_alpha_cut(0.5) == Interval.two_values(1.5, 2.5)
-    assert fn_a.get_alpha_cut(0.75) == Interval.two_values(1.75, 2.25)
-    assert fn_a.get_alpha_cut(1) == Interval.two_values(2, 2)
+
+    assert fn_a.get_alpha_cut(0) == IntervalFactory.two_values(1, 3)
+    assert fn_a.get_alpha_cut(0.25) == IntervalFactory.two_values(1.25, 2.75)
+    assert fn_a.get_alpha_cut(0.5) == IntervalFactory.two_values(1.5, 2.5)
+    assert fn_a.get_alpha_cut(0.75) == IntervalFactory.two_values(1.75, 2.25)
+    assert fn_a.get_alpha_cut(1) == IntervalFactory.two_values(2, 2)
 
 
 def test_contain(fn_a: FuzzyNumber):
-    
+
     assert 2 in fn_a
     assert 1 in fn_a
     assert 1.1 in fn_a
     assert 2.9 in fn_a
     assert 3 in fn_a
-    assert Interval(2.9, 3.1) in fn_a
-    assert FuzzyNumber.crisp_number(3) in fn_a
+    assert IntervalFactory.infimum_supremum(2.9, 3.1) in fn_a
+    assert FuzzyNumberFactory.crisp_number(3) in fn_a
     assert (0.999 in fn_a) is False
     assert (3.001 in fn_a) is False
-    
+
     with pytest.raises(TypeError,
                        match="Cannot test if object of type"):
         "a" in fn_a
-    
+
 
 def test_get_alpha_cut_values():
-    
+
     assert FuzzyNumber.get_alpha_cut_values(6, precision=4) == [0, 0.2, 0.4, 0.6, 0.8, 1]
     assert FuzzyNumber.get_alpha_cut_values(2, precision=4) == [0, 1]
     assert FuzzyNumber.get_alpha_cut_values(11, precision=8) == [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
@@ -167,83 +168,83 @@ def test_get_alpha_cut_values():
     with pytest.raises(ValueError,
                        match="`number_of_cuts` has to be integer and higher than 1"):
         FuzzyNumber.get_alpha_cut_values(1, precision=2)
-        
+
 
 def test_add(fn_a: FuzzyNumber, fn_b: FuzzyNumber, fn_c: FuzzyNumber):
-    
-    assert fn_a + 1 == FuzzyNumber.triangular(2, 3, 4)
-    
-    assert 1 + fn_a == FuzzyNumber.triangular(2, 3, 4)
-    
-    assert fn_a + fn_b == FuzzyNumber.triangular(3, 5, 7)
-    
-    assert fn_b + fn_a == FuzzyNumber.triangular(3, 5, 7)
-    
-    assert fn_a + fn_c == FuzzyNumber.triangular(0, 2, 4)
+
+    assert fn_a + 1 == FuzzyNumberFactory.triangular(2, 3, 4)
+
+    assert 1 + fn_a == FuzzyNumberFactory.triangular(2, 3, 4)
+
+    assert fn_a + fn_b == FuzzyNumberFactory.triangular(3, 5, 7)
+
+    assert fn_b + fn_a == FuzzyNumberFactory.triangular(3, 5, 7)
+
+    assert fn_a + fn_c == FuzzyNumberFactory.triangular(0, 2, 4)
 
 
 def test_sub(fn_a: FuzzyNumber, fn_b: FuzzyNumber, fn_c: FuzzyNumber):
-    
-    assert fn_a - 1 == FuzzyNumber.triangular(0, 1, 2)
-    
-    assert 1 - fn_a == FuzzyNumber.triangular(-2, -1, 0)
-    
-    assert fn_a - fn_b == FuzzyNumber.triangular(-3, -1, 1)
-    
-    assert fn_b - fn_a == FuzzyNumber.triangular(-1, 1, 3)
-    
-    assert fn_a - fn_c == FuzzyNumber.triangular(0, 2, 4)
-    
+
+    assert fn_a - 1 == FuzzyNumberFactory.triangular(0, 1, 2)
+
+    assert 1 - fn_a == FuzzyNumberFactory.triangular(-2, -1, 0)
+
+    assert fn_a - fn_b == FuzzyNumberFactory.triangular(-3, -1, 1)
+
+    assert fn_b - fn_a == FuzzyNumberFactory.triangular(-1, 1, 3)
+
+    assert fn_a - fn_c == FuzzyNumberFactory.triangular(0, 2, 4)
+
 
 def test_truediv(fn_a: FuzzyNumber, fn_b: FuzzyNumber, fn_c: FuzzyNumber):
-    
-    assert fn_a / 2 == FuzzyNumber.triangular(0.5, 1, 1.5)
-    
+
+    assert fn_a / 2 == FuzzyNumberFactory.triangular(0.5, 1, 1.5)
+
     with pytest.raises(ArithmeticError,
                        match="Cannot divide by 0"):
         fn_a / 0
-        
-    assert fn_a / fn_b == FuzzyNumber.triangular(fn_a.min / fn_b.max,
-                                                 fn_a.kernel.min / fn_b.kernel.min,
-                                                 fn_a.max / fn_b.min)
 
-    assert 5 / fn_a == FuzzyNumber.triangular(5 / fn_a.max, 5 / fn_a.kernel.min, 5 / fn_a.min)
-    
+    assert fn_a / fn_b == FuzzyNumberFactory.triangular(fn_a.min / fn_b.max,
+                                                        fn_a.kernel.min / fn_b.kernel.min,
+                                                        fn_a.max / fn_b.min)
+
+    assert 5 / fn_a == FuzzyNumberFactory.triangular(5 / fn_a.max, 5 / fn_a.kernel.min, 5 / fn_a.min)
+
 
 def test_pow(fn_a: FuzzyNumber):
-    
+
     power = 2
-    
-    assert pow(fn_a, power) == FuzzyNumber.triangular(1**power, 2**power, 3**power)
+
+    assert pow(fn_a, power) == FuzzyNumberFactory.triangular(1**power, 2**power, 3**power)
 
 
 def test_function():
-    
-    fn = FuzzyNumber.triangular(-math.pi / 2, 0, math.pi / 2, 11, 8)
+
+    fn = FuzzyNumberFactory.triangular(-math.pi / 2, 0, math.pi / 2, 11, 8)
 
     with pytest.raises(ValueError,
                        match="`function` must be either"):
         fn.apply_function(5)
-    
+
     fn_cos = fn.apply_function(math.cos)
 
     diff = 0.00000001
-    
+
     assert fn_cos.min == pytest.approx(-0, diff)
     assert fn_cos.max == pytest.approx(1, diff)
     assert fn_cos.kernel_min == pytest.approx(1, diff)
-    
+
     fn_sin = fn.apply_function(math.sin)
 
     assert fn_sin.min == pytest.approx(-1, diff)
     assert fn_sin.max == pytest.approx(1, diff)
     assert fn_sin.kernel_min == pytest.approx(0, diff)
-    
+
 
 def test_comparisons(fn_a: FuzzyNumber, fn_b: FuzzyNumber, fn_c: FuzzyNumber):
 
     assert (fn_a == fn_b) is False
-    assert fn_a == FuzzyNumber.triangular(1, 2, 3)
+    assert fn_a == FuzzyNumberFactory.triangular(1, 2, 3)
 
     assert fn_c < fn_b
     assert (fn_c > fn_b) is False
@@ -258,45 +259,45 @@ def test_comparisons(fn_a: FuzzyNumber, fn_b: FuzzyNumber, fn_c: FuzzyNumber):
 
 
 def test_complex_comparisons():
-    
-    fn_a = FuzzyNumber.triangular(2, 3, 5)
-    fn_b = FuzzyNumber.triangular(1.5, 4, 4.8)
+
+    fn_a = FuzzyNumberFactory.triangular(2, 3, 5)
+    fn_b = FuzzyNumberFactory.triangular(1.5, 4, 4.8)
 
     diff = 0.000000001
 
     assert fn_a.possibility_exceedance(fn_b) == pytest.approx(0.7777777777777777, diff)
-    
+
     assert fn_a.necessity_exceedance(fn_b) == pytest.approx(0.4285714285714289, diff)
 
     assert fn_a.possibility_strict_exceedance(fn_b) == pytest.approx(0.357142857142857, diff)
-    
+
     assert fn_a.necessity_strict_exceedance(fn_b) == pytest.approx(0.0, diff)
-    
+
     assert fn_a.possibility_undervaluation(fn_b) == pytest.approx(1.0, diff)
-    
+
     assert fn_a.necessity_undervaluation(fn_b) == pytest.approx(0.6428571428571429, diff)
-    
+
     assert fn_a.possibility_strict_undervaluation(fn_b) == pytest.approx(0.5714285714285711, diff)
-    
+
     assert fn_a.necessity_strict_undervaluation(fn_b) == pytest.approx(0.22222222222222235, diff)
 
-    fn_a = FuzzyNumber.triangular(1.7, 2.7, 2.8)
-    fn_b = FuzzyNumber.triangular(0, 1.8, 2.2)
+    fn_a = FuzzyNumberFactory.triangular(1.7, 2.7, 2.8)
+    fn_b = FuzzyNumberFactory.triangular(0, 1.8, 2.2)
 
     assert fn_a.possibility_exceedance(fn_b) == pytest.approx(1.0, diff)
-    
+
     assert fn_a.necessity_exceedance(fn_b) == pytest.approx(0.9642857142857143, diff)
-    
+
     assert fn_a.possibility_strict_exceedance(fn_b) == pytest.approx(1.0, diff)
-    
+
     assert fn_a.necessity_strict_exceedance(fn_b) == pytest.approx(0.642857142857143, diff)
-    
+
     assert fn_a.possibility_undervaluation(fn_b) == pytest.approx(0.35714285714285726, diff)
-    
+
     assert fn_a.necessity_undervaluation(fn_b) == pytest.approx(0.0, diff)
-    
+
     assert fn_a.possibility_strict_undervaluation(fn_b) == pytest.approx(0.03571428571428574, diff)
-    
+
     assert fn_a.necessity_strict_undervaluation(fn_b) == pytest.approx(0.0, diff)
 
 
@@ -308,7 +309,7 @@ def test_hash(i_a: FuzzyNumber):
 
 
 def test_repr(i_a: FuzzyNumber):
-    
+
     assert isinstance(i_a.__repr__(), str)
 
 
--- tests/test_interval.py
+++ tests/test_interval.py
@@ -2,17 +2,18 @@ import math
 import pytest
 
 from FuzzyMath.class_interval import Interval
+from FuzzyMath.class_factories import IntervalFactory
 
 
 def test_creation_errors():
 
     with pytest.raises(ArithmeticError,
                        match="`width` of interval must number higher or at least equal to 0."):
-        Interval.midpoint_width(0, -1)
+        IntervalFactory.midpoint_width(0, -1)
 
     with pytest.raises(ValueError,
                        match="The interval is invalid. `minimum` must be lower or equal to `maximum`"):
-        Interval.infimum_supremum(3, 1)
+        IntervalFactory.infimum_supremum(3, 1)
 
     with pytest.raises(ValueError,
                        match="`precision` should be value from range `0` to `15`"):
@@ -25,59 +26,59 @@ def test_creation_errors():
 
     with pytest.raises(ValueError,
                        match="Cannot parse Interval from this definition"):
-        Interval.parse_string("[1, 2.5, 5]")
+        IntervalFactory.parse_string("[1, 2.5, 5]")
 
     with pytest.raises(ValueError,
                        match="Cannot parse Interval from this definition"):
-        Interval.parse_string("[]")
+        IntervalFactory.parse_string("[]")
 
     with pytest.raises(ValueError,
                        match="Cannot parse Interval from this definition"):
-        Interval.parse_string("[\"aa\", \"b\"]")
+        IntervalFactory.parse_string("[\"aa\", \"b\"]")
 
 
 def test_creation():
 
-    assert isinstance(Interval.infimum_supremum(1, 3), Interval)
+    assert isinstance(IntervalFactory.infimum_supremum(1, 3), Interval)
 
-    assert isinstance(Interval.infimum_supremum(2, 5), Interval)
+    assert isinstance(IntervalFactory.infimum_supremum(2, 5), Interval)
 
-    assert isinstance(Interval.infimum_supremum(4, 7), Interval)
+    assert isinstance(IntervalFactory.infimum_supremum(4, 7), Interval)
 
-    assert isinstance(Interval.infimum_supremum(-2, 3), Interval)
+    assert isinstance(IntervalFactory.infimum_supremum(-2, 3), Interval)
 
-    assert isinstance(Interval.infimum_supremum(-1, 1), Interval)
+    assert isinstance(IntervalFactory.infimum_supremum(-1, 1), Interval)
 
-    assert isinstance(Interval.empty(), Interval)
+    assert isinstance(IntervalFactory.empty(), Interval)
 
-    interval = Interval.two_values(1.0000000000001, 3.000000009, precision=2)
+    interval = IntervalFactory.two_values(1.0000000000001, 3.000000009, precision=2)
 
-    assert interval == Interval.two_values(1, 3, precision=2)
+    assert interval == IntervalFactory.two_values(1, 3, precision=2)
 
-    interval = Interval.parse_string("[1, 2.5]")
+    interval = IntervalFactory.parse_string("[1, 2.5]")
 
     assert interval.min == 1
     assert interval.max == 2.5
 
 
 def test_degenerate_interval():
-    
-    assert Interval.infimum_supremum(2, 2).degenerate
-    assert Interval.infimum_supremum(2, 3).degenerate is False
+
+    assert IntervalFactory.infimum_supremum(2, 2).degenerate
+    assert IntervalFactory.infimum_supremum(2, 3).degenerate is False
 
 
 def test_mid_point():
-    
-    a = Interval.two_values(1, 2)
+
+    a = IntervalFactory.two_values(1, 2)
     assert a.mid_point == 1.5
 
-    b = Interval.midpoint_width(2, 2)
+    b = IntervalFactory.midpoint_width(2, 2)
     assert b.mid_point == 2
 
 
 def test_contains():
 
-    interval = Interval.infimum_supremum(1, 5)
+    interval = IntervalFactory.infimum_supremum(1, 5)
 
     assert 2 in interval
     assert 1 in interval
@@ -86,10 +87,9 @@ def test_contains():
     assert (0.999999 in interval) is False
     assert (5.0001 in interval) is False
 
-    print(Interval.two_values(6, 7) in interval)
-    assert (Interval.two_values(6, 7) in interval) is False
-    assert (Interval.two_values(0, 3) in interval) is False
-    assert Interval.two_values(2, 4) in interval
+    assert (IntervalFactory.two_values(6, 7) in interval) is False
+    assert (IntervalFactory.two_values(0, 3) in interval) is False
+    assert IntervalFactory.two_values(2, 4) in interval
 
     with pytest.raises(TypeError,
                        match="Cannot test if object of type "):
@@ -109,19 +109,19 @@ def test_intersects(i_a: Interval, i_b: Interval, i_c: Interval):
 
 
 def test_intersection(i_a: Interval, i_b: Interval, i_c: Interval):
-    
-    assert i_a.intersection(i_b) == Interval.two_values(2, 3)
-    assert i_b.intersection(i_c) == Interval.two_values(4, 5)
-    
+
+    assert i_a.intersection(i_b) == IntervalFactory.two_values(2, 3)
+    assert i_b.intersection(i_c) == IntervalFactory.two_values(4, 5)
+
     with pytest.raises(ArithmeticError,
                        match="do not intersect"):
         i_a.intersection(i_c)
 
 
 def test_union(i_a, i_b, i_c):
-    
-    assert i_a.union(i_b) == Interval.two_values(1, 5)
-    assert i_b.union(i_c) == Interval.two_values(2, 7)
+
+    assert i_a.union(i_b) == IntervalFactory.two_values(1, 5)
+    assert i_b.union(i_c) == IntervalFactory.two_values(2, 7)
 
     with pytest.raises(ArithmeticError,
                        match="do not intersect"):
@@ -129,18 +129,18 @@ def test_union(i_a, i_b, i_c):
 
 
 def test_union_hull(i_a: Interval, i_c: Interval):
-    assert i_a.union_hull(i_c) == Interval.two_values(1, 7)
+    assert i_a.union_hull(i_c) == IntervalFactory.two_values(1, 7)
 
 
 def test_add(i_a: Interval, i_b: Interval, i_e: Interval):
-    
-    assert i_a + 1 == Interval.two_values(i_a.min + 1, i_a.max + 1)
-    
-    assert 1 + i_a == Interval.two_values(i_a.min + 1, i_a.max + 1)
-    
-    assert i_a + i_b == Interval.two_values(i_a.min + i_b.min, i_a.max + i_b.max)
-        
-    assert i_a + i_e == Interval.two_values(i_a.min + i_e.min, i_a.max + i_e.max)
+
+    assert i_a + 1 == IntervalFactory.two_values(i_a.min + 1, i_a.max + 1)
+
+    assert 1 + i_a == IntervalFactory.two_values(i_a.min + 1, i_a.max + 1)
+
+    assert i_a + i_b == IntervalFactory.two_values(i_a.min + i_b.min, i_a.max + i_b.max)
+
+    assert i_a + i_e == IntervalFactory.two_values(i_a.min + i_e.min, i_a.max + i_e.max)
 
     with pytest.raises(TypeError,
                        match="unsupported operand"):
@@ -149,13 +149,13 @@ def test_add(i_a: Interval, i_b: Interval, i_e: Interval):
 
 
 def test_sub(i_a: Interval, i_b: Interval):
-    
-    assert i_a - 1 == Interval.two_values(i_a.min - 1, i_a.max - 1)
-    
-    assert 1 - i_a == Interval.two_values(1 - i_a.min, 1 - i_a.max)
-    
-    assert i_a - i_b == Interval.two_values(i_a.min - i_b.max, i_a.max - i_b.min)
-    
+
+    assert i_a - 1 == IntervalFactory.two_values(i_a.min - 1, i_a.max - 1)
+
+    assert 1 - i_a == IntervalFactory.two_values(1 - i_a.min, 1 - i_a.max)
+
+    assert i_a - i_b == IntervalFactory.two_values(i_a.min - i_b.max, i_a.max - i_b.min)
+
     with pytest.raises(TypeError,
                        match="unsupported operand"):
         i_a - "str"
@@ -163,13 +163,13 @@ def test_sub(i_a: Interval, i_b: Interval):
 
 
 def test_mul(i_a: Interval, i_b: Interval):
-    
-    assert i_a * 2 == Interval.two_values(i_a.min * 2, i_a.max * 2)
-    
-    assert 2 * i_a == Interval.two_values(i_a.min * 2, i_a.max * 2)
-    
-    assert i_a * i_b == Interval.two_values(i_a.min * i_b.min, i_a.max * i_b.max)
-    
+
+    assert i_a * 2 == IntervalFactory.two_values(i_a.min * 2, i_a.max * 2)
+
+    assert 2 * i_a == IntervalFactory.two_values(i_a.min * 2, i_a.max * 2)
+
+    assert i_a * i_b == IntervalFactory.two_values(i_a.min * i_b.min, i_a.max * i_b.max)
+
     with pytest.raises(TypeError,
                        match="multiply sequence"):
         i_a * "str"
@@ -177,13 +177,13 @@ def test_mul(i_a: Interval, i_b: Interval):
 
 
 def test_truediv(i_a: Interval, i_b: Interval, i_d: Interval):
-    
-    assert i_a / 2 == Interval.two_values(i_a.min / 2, i_a.max / 2)
 
-    assert 2 / i_a == Interval.two_values(2 / i_a.min, 2 / i_a.max)
-     
-    assert i_a / i_b == Interval.two_values(i_a.min / i_b.max, i_a.max / i_b.min)
-      
+    assert i_a / 2 == IntervalFactory.two_values(i_a.min / 2, i_a.max / 2)
+
+    assert 2 / i_a == IntervalFactory.two_values(2 / i_a.min, 2 / i_a.max)
+
+    assert i_a / i_b == IntervalFactory.two_values(i_a.min / i_b.max, i_a.max / i_b.min)
+
     with pytest.raises(TypeError,
                        match="unsupported operand"):
         i_a / "str"
@@ -199,60 +199,60 @@ def test_truediv(i_a: Interval, i_b: Interval, i_d: Interval):
 
 def test_pow(i_a: Interval, i_d: Interval):
 
-    assert i_a ** 2 == Interval.two_values(1, 9)
-    
-    assert i_a ** 3 == Interval.two_values(1, 27)
+    assert i_a ** 2 == IntervalFactory.two_values(1, 9)
 
-    assert i_d ** 2 == Interval.two_values(0, 9)
-    
-    assert i_d ** 3 == Interval.two_values(-8, 27)
+    assert i_a ** 3 == IntervalFactory.two_values(1, 27)
+
+    assert i_d ** 2 == IntervalFactory.two_values(0, 9)
+
+    assert i_d ** 3 == IntervalFactory.two_values(-8, 27)
 
 
 def test_neg(i_a: Interval, i_d: Interval):
-    
-    assert -i_a == Interval.two_values(i_a.min * (-1), i_a.max * (-1))
-    
-    assert -i_d == Interval.two_values(i_d.min * (-1), i_d.max * (-1))
+
+    assert -i_a == IntervalFactory.two_values(i_a.min * (-1), i_a.max * (-1))
+
+    assert -i_d == IntervalFactory.two_values(i_d.min * (-1), i_d.max * (-1))
 
 
 def test_eq(i_a: Interval, i_b: Interval):
-    
+
     assert i_a == i_a
-    
+
     assert (i_a == i_b) is False
 
 
 def test_lt(i_a: Interval, i_b: Interval, i_c: Interval, i_d: Interval):
-    
+
     assert i_a < i_c
-    
+
     assert i_d < i_c
-    
+
     assert (i_a < i_b) is False
-    
-    
+
+
 def test_gt(i_a: Interval, i_b: Interval, i_c: Interval, i_d: Interval):
-    
+
     assert i_c > i_a
-    
+
     assert i_c > i_d
-    
+
     assert (i_b > i_a) is False
-    
-    
+
+
 def test_lt_gt(i_a: Interval, i_b: Interval, i_c: Interval, i_d: Interval):
-    
+
     assert (i_a < i_c) == (i_c > i_a)
-    
+
     assert (i_d < i_c) == (i_c > i_d)
-    
- 
+
+
 def test_is_empty(i_a: Interval, i_f: Interval):
-     
+
     assert i_f.is_empty
-    
+
     assert i_a.is_empty is False
-    
+
 
 def test_apply_function(i_a: Interval, i_b: Interval):
 
@@ -267,11 +267,11 @@ def test_apply_function(i_a: Interval, i_b: Interval):
         i_a.apply_function(math.log2, c=5)
 
     assert i_a.apply_function(math.log2).min == pytest.approx(math.log2(i_a.min), 0.00001)
-    
+
     assert i_a.apply_function(math.log2).max == pytest.approx(math.log2(i_a.max), 0.00001)
-    
+
     assert i_b.apply_function(math.cos).min == pytest.approx(-1, 0.00001)
-    
+
     assert i_b.apply_function(math.cos).max == pytest.approx(0.28366, 0.00001)
-    
-    assert i_a.apply_function(math.pow, 2, number_elements=10) == Interval.two_values(1, 9)
+
+    assert i_a.apply_function(math.pow, 2, number_elements=10) == IntervalFactory.two_values(1, 9)
