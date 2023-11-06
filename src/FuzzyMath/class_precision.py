ddfe2ab9dbfd754c693e648380aff9123df59a39
Jan Caha <jan.caha@outlook.com>
2023-11-06 10:47:02 +0100

    docstrings

--- src/FuzzyMath/class_precision.py
+++ src/FuzzyMath/class_precision.py
@@ -3,6 +3,8 @@ from decimal import Decimal
 
 
 class FuzzyMathPrecision(object):
+    """Object represeting precision of FuzzyMath. Used in Interval and FuzzyNumber representation."""
+
     numeric_precision: typing.Optional[Decimal] = None
     alpha_precision: typing.Optional[Decimal] = None
 
@@ -13,22 +15,42 @@ class FuzzyMathPrecision(object):
 
     @staticmethod
     def set_numeric_precision(decimal_places: int) -> None:
+        """Set precision for numeric values - typically limits of Interval and thus Alpha cuts as well.
+
+        Args:
+            decimal_places (int): Number of decimal places to use.
+        """
         FuzzyMathPrecision().numeric_precision = Decimal(10) ** -decimal_places
 
     @staticmethod
     def unset_numeric_precision() -> None:
+        """Unset decimal places precision."""
         FuzzyMathPrecision().numeric_precision = None
 
     @staticmethod
     def set_alpha_precision(decimal_places: int) -> None:
+        """Set alpha values precision.  Limits the precision of alpha values.
+
+        Args:
+            decimal_places (int): Number of decimal places to use for alpha cut values.
+        """
         FuzzyMathPrecision().alpha_precision = Decimal(10) ** -decimal_places
 
     @staticmethod
     def unset_alpha_precision() -> None:
+        """Unset decimal places for alpha precision."""
         FuzzyMathPrecision().alpha_precision = None
 
     @staticmethod
     def prepare_number(value: Decimal) -> Decimal:
+        """Prepare number according to current settings of FuzzyMathPrecision.
+
+        Args:
+            value (Decimal): Input value.
+
+        Returns:
+            Decimal
+        """
         fuzzy_precision = FuzzyMathPrecision()
         if fuzzy_precision.numeric_precision is None:
             return value
@@ -37,6 +59,14 @@ class FuzzyMathPrecision(object):
 
     @staticmethod
     def prepare_alpha(value: Decimal) -> Decimal:
+        """Prepare alpha value according to current settings of FuzzyMathPrecision.
+
+        Args:
+            value (Decimal): Input alpha value.
+
+        Returns:
+            Decimal
+        """
         fuzzy_precision = FuzzyMathPrecision()
         if fuzzy_precision.alpha_precision is None:
             return value
