8fc902468669497cdb7b91f9c74abd4aec6fc643
Jan Caha <jan.caha@outlook.com>
2023-11-07 21:39:44 +0100

    typos

--- src/FuzzyMath/class_fuzzy_number.py
+++ src/FuzzyMath/class_fuzzy_number.py
@@ -500,7 +500,7 @@ class FuzzyNumber:
             Function to apply to fuzzy number.
 
         args
-            Postional arguments for the `function`.
+            Positional arguments for the `function`.
 
         monotone: bool
             Is the function monotone? Default `False`. If `True` can significantly speed up calculation.
@@ -634,7 +634,7 @@ class FuzzyNumber:
         order_by_alphas_from_one: bool = False,
     ) -> List[Decimal]:
         """
-        Extract minimimal values of provided alpha cuts as list.
+        Extract minimal values of provided alpha cuts as list.
 
         Parameters
         ----------
